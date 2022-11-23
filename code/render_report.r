5f9695a9e7b682b04457edf57fb605809d7fbb9e
David Benkeser <benkeser@emory.edu>
2022-11-23 10:03:03 -0500

    Update render_report.r
    the code breaks gracefully, but I think this is what you were going for?

--- code/render_report.r
+++ code/render_report.r
@@ -2,7 +2,9 @@ here::i_am(
   "code/render_report.r"
 )
 
+WHICH_CONFIG <- Sys.getenv('WHICH_CONFIG')
+
 rmarkdown::render(
   here::here("engage_report.Rmd"),
-  output_file = 'INFO550_EngagementReport_Group2_${WHICH_CONFIG}.html'
+  output_file = paste0('INFO550_EngagementReport_Group2_', WHICH_CONFIG, '.html')
 )
